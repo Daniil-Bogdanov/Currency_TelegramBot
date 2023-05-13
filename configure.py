48df46c8e5c69eafbe5d115ef2569231d0c82df3
Daniil <dyatel.vudi.23@gmail.com>
2023-05-13 21:50:39 +0300

    release

---
+++ configure.py
@@ -0,0 +1,10 @@
+config = {
+    'name': 'curssssbot',
+    'token': '5676242659:AAFDoHfnooKIgaHeTuWBJzcaOGgX0u1auSM'
+}
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
